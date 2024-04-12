d0dd8ee8e95fff2df2ead63ad9f52664e169bfe1
mnenie <1alexpeshkov@gmail.com>
2024-04-12 14:17:21 +0300

    add: shadcn + prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  shadcn: {
+    prefix: 'Ui',
+    componentDir: './components/ui'
+  }
 })
