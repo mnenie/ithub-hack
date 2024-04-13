79b37fff176aae5da178ac55fd8ec8fe3b7bbd99
mnenie <121057011+mneniee@users.noreply.github.com>
2024-04-14 00:28:15 +0300

    fix: ai + messages

--- pages/ai.vue
+++ pages/ai.vue
@@ -4,7 +4,6 @@ useSeoMeta({
 });
 
 const model = ref('');
-const userMessage: { role: "user"; text: string; } = { role: "user", text: "" };
 
 const messages = ref<ChatCompletionRequestMessage[]>([]);
 
@@ -15,11 +14,11 @@ const { events } = storeToRefs(useEventsStore());
 const { usePostConversations } = useConversations(courses.value, canteens.value, events.value, model);
 
 const handleSubmit = async () => {
-  userMessage.text = model.value;
-  messages.value.push(userMessage); 
+  const userMessage: { role: 'user'; text: string } = { role: 'user', text: model.value };
+  messages.value.push(userMessage);
   const response = await usePostConversations();
   messages.value.push(response);
-  model.value = ''
+  model.value = '';
 };
 </script>
 
@@ -27,15 +26,11 @@ const handleSubmit = async () => {
   <div class="absolute bottom-0 left-0 right-0">
     <div class="relative flex h-full w-full flex-col justify-end self-end px-96">
       <div class="flex w-full flex-col items-start justify-center">
-        <div
-          v-for="(message, index) in messages"
-          :key="index"
-          class="mb-8 flex items-center gap-4"
-        >
+        <div v-for="(message, index) in messages" :key="index" class="mb-8 flex items-center gap-4">
           <UiAvatar>
             <UiAvatarFallback>{{ message && message.role === 'user' ? '1a' : 'ai' }}</UiAvatarFallback>
           </UiAvatar>
-          <div v-html="message && message.text"></div>
+          <div v-html="message ? message.text : ''"></div>
         </div>
         <AiMessage @on-submit="handleSubmit" v-model="model" />
       </div>
